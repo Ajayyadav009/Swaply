c8619b800a35a2547d24ac601e92f9a3a232a96a
Ajay Yadav <yadavajay39224@gmail.com>
2026-06-06 02:16:38 +0530

    session

---
+++ server/models/Avalability.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const avalabiltySchema = new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required: true
+
+    },
+    dayOfWeek:{
+        type:Number,
+        required: true,
+        min:0,
+        max:6
+    },
+    startTime:{
+        type: String,
+        required:true
+    },
+    endTime:{
+        type: String,
+        required: true
+    },
+    timezone:{
+        type: String,
+        default:'Asia/kolkata'
+    },
+    isRecurring:{
+        type:Boolean,
+        default:true
+    },
+    isActive:{
+        type:Boolean,
+        default:true
+    },
+
+},
+{ timestamps: true }
+);
+avalabiltySchema = index({ user: 1,  dayOfWeek: 1});
+module.exports = mongoose.model('Avalibilty', avalabiltySchema);
---
+++ server/models/Session.js
@@ -0,0 +1,68 @@
+const mongoose = required("mongoose");
+const sessionSchema = new mongoose.Schema(
+  {
+    teacher: {
+      type: mongoose.Schema.Types.Objectid,
+      ref: "User",
+      required: true,
+    },
+    student: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    skill: {
+      type: String,
+      required: true,
+    },
+    scheduledAt: {
+      type: Date,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      dafault: 60,
+    },
+    status: {
+      type: String,
+      enum: [
+        "pending",
+        "confirmed",
+        "ongoing",
+        "completed",
+        "cancelled",
+        "reschduled",
+      ],
+      default: "pending",
+    },
+    roomId: {
+      type: String,
+    },
+    notes: {
+      type: String,
+      default: "",
+    },
+    topicsCovered: [
+      {
+        type: String,
+      },
+    ],
+    feedback: {
+      teachingQuality: { type: Number, min: 1, max: 5 },
+      communication: { type: Number, min: 1, max: 5 },
+      comment: { type: String },
+      givenBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    },
+    actualStartTime: Date,
+    actualEndTime: Date,
+    cancelReason: String,
+  },
+  { timestamps: true },
+);
+
+sessionSchema.index({ teacher: 1, scheduledAt: -1 });
+sessionSchema.index({ student: 1, scheduledAt: -1 });
+module.exports = mongoose.model("Session", sessionSchema);
